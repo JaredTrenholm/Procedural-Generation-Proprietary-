6d12cfa6b7ff29faf69c396394d5e8e1aaa7ee4d
Jared Trenholm <Jared.Trenholm12@gmail.com>
2021-11-05 13:21:38 -0300

    [ADDED] Depth Layers are now created

--- Assets/MapGenerator.cs
+++ Assets/MapGenerator.cs
@@ -1,17 +1,21 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public Material topLayerMaterial;
+    public Material middleLayerMaterial;
     public float width;
     public float length;
-
+    public float maxDepth;
     public float freq;
     public float amp;
+    public string seed;
+    public bool randomSeed;
+    private List<GameObject> topLayerCubes = new List<GameObject>();
+    private List<GameObject> middleLayerCubes = new List<GameObject>();
 
-    private string seed;
-    private List<GameObject> cubes = new List<GameObject>();
+   
     void Start()
     {
         StartGeneration();
@@ -20,42 +24,71 @@ public class MapGenerator : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            StartGeneration();
+              StartGeneration();
         }
     }
     private void StartGeneration()
     {
-        foreach(GameObject cube in cubes)
+        DestroyPreviousLevel();
+        ActivateSeed(GetSeed());
+        PlaceBlocks();
+    }
+    private void DestroyPreviousLevel()
+    {
+        foreach (GameObject cube in topLayerCubes)
         {
             GameObject.Destroy(cube);
         }
-        ActivateSeed(CreateSeed());
-        PlaceBlocks();
+        foreach (GameObject cube in middleLayerCubes)
+        {
+            GameObject.Destroy(cube);
+        }
+        topLayerCubes.Clear();
+        middleLayerCubes.Clear();
     }
     private void PlaceBlocks()
     {
-        for(float x = 0; x < width; x++)
+        CreateTopLayer();
+        CreateMiddleLayer();
+    }
+    private void CreateTopLayer()
+    {
+        for (float x = 0; x < width; x++)
         {
             for (float z = 0; z < length; z++)
             {
                 GameObject cubeCreated = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cubeCreated.transform.position = new Vector3(x, PerlinNoise(x+ Time.time, z+Time.time), z);
-                cubes.Add(cubeCreated);
-                cubeCreated.transform.localScale = new Vector3(1.125f, 1.125f, 1.125f);
+                cubeCreated.transform.position = new Vector3(x, PerlinNoise(x / 2, z / 2) + PerlinNoise(z / 2, x / 2), z);
+                cubeCreated.GetComponent<MeshRenderer>().material = topLayerMaterial;
+                topLayerCubes.Add(cubeCreated);
             }
         }
     }
-    private int CreateSeed()
+    private void CreateMiddleLayer()
     {
-        seed = "" + Random.Range(int.MinValue, int.MaxValue);
+        foreach(GameObject topCube in topLayerCubes)
+        {
+            for (float depth = 1; depth < maxDepth; depth++)
+            {
+                GameObject cubeCreated = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cubeCreated.transform.position = new Vector3(topCube.transform.position.x, topCube.transform.position.y - depth, topCube.transform.position.z);
+                cubeCreated.GetComponent<MeshRenderer>().material = middleLayerMaterial;
+                middleLayerCubes.Add(cubeCreated);
+            }
+        }
+    }
+    private float PerlinNoise(float x, float y)
+    {
+        return ((Mathf.PerlinNoise(x/Random.Range(1,11), y / Random.Range(1, 11)) *freq)*amp);
+    }
+    private int GetSeed()
+    {
+        if(randomSeed)
+            seed = "" + Random.Range(int.MinValue, int.MaxValue);
         return seed.GetHashCode();
     }
     private void ActivateSeed(int seedCode)
     {
         Random.InitState(seedCode);
-    }
-    private float PerlinNoise(float x, float y)
-    {
-        return (((Mathf.PerlinNoise(x, y)*freq)-amp)*2)-1;
     }
 }
